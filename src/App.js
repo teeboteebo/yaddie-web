be3fa92bce7ae5d0d39df07a851516d5700117d5
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-09-09 11:47:15 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss'
+import { Input } from 'reactstrap'
 
 function App() {
   return (
     <div className="App">
       <h1>Yaddie!!!</h1>
+      <Input />
     </div>
   );
 }
