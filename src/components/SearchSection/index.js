877ae5692d7fef9abd4b8f125b2b68ba5b840563
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-09-10 13:59:39 +0200

    fix button

--- src/components/SearchSection/index.js
+++ src/components/SearchSection/index.js
@@ -1,27 +1,30 @@
-import React from "react";
-import { Col, Button, Form, FormGroup, Input } from "reactstrap";
-import "./style.scss";
+import React from 'react';
+import { Col, Button, Form, FormGroup, Input } from 'reactstrap';
+import './style.scss';
 
 class SearchSection extends React.Component {
   render() {
     return (
-      <section className="search-section">
+      <section className='search-section'>
         <Form>
           <FormGroup>
             <Col sm={12}>
               <Input
-                className="search-bar"
-                type="text"
-                name="search-bar"
-                placeholder="Sök efter recept.."
+                className='search-bar'
+                type='text'
+                name='search-bar'
+                placeholder='Sök efter recept..'
               />
             </Col>
           </FormGroup>
         </Form>
         <div>
-          <Button color="primary" className="text-white">
+          <a
+            href='/nytt-recept'
+            type='btn'
+            className='btn btn-primary add-recipes text-white'>
             Lägg till recept!
-          </Button>
+          </a>
         </div>
       </section>
     );
