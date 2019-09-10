b1f6bc7f363e2911082175b25e537123ce71c969
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-09-10 11:35:05 +0200

    Logo image
    Not fully fixed but logo exist..

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles.scss'
+import logo from '../../img/yaddie-logo-orange.png'
 
 class Header extends React.Component {
   render(){
     return(
       <header>
         Header
+        <img class="logo" src={logo} alt={"logo"}/> 
       </header>
     )
   }
