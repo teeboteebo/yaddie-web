da3e74efe073924410bc78668a4bf25d775dadd7
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-09-10 12:26:05 +0200

    structure for searchbar
    not yet fixed, just a testing/view of how it's gonna look

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './styles.scss'
+import { Row, Col, } from 'reactstrap';
 import SearchSection from '../SearchSection';
 
 class Header extends React.Component {
@@ -7,7 +8,18 @@ class Header extends React.Component {
     return(
       <header>
         Header
-        <SearchSection />
+        <Row className="justify-content-md-center">
+          <Col xs="3">
+            1 of 3
+          </Col>
+          <Col md="6"><SearchSection /></Col>
+          <Col xs="3">
+            3 of 3
+          </Col>
+        </Row>
+
+        
+
       </header>
     )
   }
