5bed031d9018c4970f9d27ee631d82c2406a80ed
joel <joel.edling1@gmail.com>
2019-09-10 11:11:59 +0200

    added search bar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Button } from 'reactstrap'
-import './style.scss'
+import React from "react";
+import { Col, Row, Button, Form, FormGroup, Input } from "reactstrap";
+import "./style.scss";
 
 class SearchBar extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <article className="search-bar">
-        search SearchBar
+        <Form>
+          <FormGroup row>
+            <Col sm={10}>
+              <Input
+                type="text"
+                name="search-bar"
+                placeholder="Sök efter recept.."
+              />
+            </Col>
+          </FormGroup>
+        </Form>
       </article>
-    )
+    );
   }
 }
-export default SearchBar
+export default SearchBar;
