68194d1284e7ffdc1e3e6d7b8ab3b44e4f784af8
joel <joel.edling1@gmail.com>
2019-09-10 12:28:12 +0200

    border-radius, and styling of searchbar

--- src/components/SearchSection/index.js
+++ src/components/SearchSection/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Col, Button, Form, FormGroup, Input } from 'reactstrap';
-import './style.scss';
+import React from "react";
+import { Col, Button, Form, FormGroup, Input } from "reactstrap";
+import "./style.scss";
 
 class SearchSection extends React.Component {
   render() {
     return (
-      <section className='search-section'>
+      <section className="search-section">
         <Form>
           <FormGroup>
-            <Col sm={10}>
+            <Col sm={12}>
               <Input
-                type='text'
-                name='search-bar'
-                placeholder='Sök efter recept..'
+                className="search-bar"
+                type="text"
+                name="search-bar"
+                placeholder="Sök efter recept.."
               />
             </Col>
           </FormGroup>
         </Form>
         <div>
-          <Button color='primary' className='text-white'>
+          <Button color="primary" className="text-white">
             Lägg till recept!
           </Button>
         </div>
