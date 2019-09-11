eb4a84223ca16eaeebc7bd47929a5f9a274e8aa4
joel <joel.edling1@gmail.com>
2019-09-11 11:33:11 +0200

    Auto stash before merge of "feature/searchbar" and "origin/feature/searchbar"

--- src/components/SearchSection/index.js
+++ src/components/SearchSection/index.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { Col, Form, FormGroup, Input } from 'reactstrap';
-import './style.scss';
+import React from "react";
+import { Col, Input, Button } from "reactstrap";
+import "./style.scss";
 
 class SearchSection extends React.Component {
   render() {
     return (
-      <section className='search-section'>
-        <Form>
-          <FormGroup>
-            <Col sm={12}>
-              <Input
-                className='search-bar'
-                type='text'
-                name='search-bar'
-                placeholder='Sök efter recept..'
-              />
-            </Col>
-          </FormGroup>
-        </Form>
+      <section className="search-section">
+        <Col sm={12} className="button-inside">
+          <Input
+            className="search-bar"
+            type="text"
+            name="search"
+            placeholder="Sök efter recept.."
+          />
+          <Button type="btn" color="success" className="search-button">
+            Sök
+          </Button>
+        </Col>
         <a
-          href='/till-receptvyn'
-          type='btn'
-          className='btn btn-success search-by-category text-white'>
+          href="/till-receptvyn"
+          type="btn"
+          className="btn btn-success search-by-category text-white"
+        >
           Sök efter kategori!
         </a>
         <a
-          href='/nytt-recept'
-          type='btn'
-          className='btn btn-primary add-recipes text-white float-right'>
+          href="/nytt-recept"
+          type="btn"
+          className="btn btn-primary add-recipes text-white float-right"
+        >
           Lägg till recept!
         </a>
       </section>
