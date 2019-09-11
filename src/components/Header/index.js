8eade55c3fdfbf80ee1dd276e9bf50f724df1fbb
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-09-11 10:05:29 +0200

    changes lines for logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import './styles.scss'
+import React from 'react';
+import './styles.scss';
 import { Row, Col, } from 'reactstrap';
 import SearchSection from '../SearchSection';
-import logo from '../../img/yaddie-logo-orange.png'
+import logo from '../../img/yaddie-logo-orange.png';
 
 class Header extends React.Component {
   render(){
     return(
       <header>
           <Row className="justify-content-md-center">
+            <Col md="2"><img className="logo" src={logo} alt={"logo"}/> </Col>
           <Col md="8"> <SearchSection /></Col>
         </Row>
-
         
-
-        Header
-        <img class="logo" src={logo} alt={"logo"}/> 
       </header>
     )
   }
