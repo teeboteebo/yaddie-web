f21d33af3cc219d3c87e656265371108fa16fc75
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-09-13 12:12:05 +0200

    Footer insert..
    it's a footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import './styles.scss'
+import { Row, Col, Container} from 'reactstrap';
+import logo from '../../img/yaddie-logo-orange.png';
+
 
 class Footer extends React.Component {
   render(){
     return(
       <footer>
-        Footer
-      </footer>
+      <Container>
+    <Row className="text-center py-4">
+      <Col md={6}>
+        <div className="info">
+        <h5 className="bold">Yaddie</h5>
+        <p>1355 Market St,Suite 900</p>
+        <p>San Fransisco, CA 94103</p>
+        <p>P:(123) 456-7890</p>
+        </div>
+        <div className="Contact">
+          <h5 className="bold">Kontakt</h5>
+          <p>yaddie@gmail.com</p>
+        </div>
+       
+      </Col>
+      <Col md={6}>
+      <img className="mx-auto d-block logo-footer" src={logo} alt={"logo"}/>
+
+      </Col>
+    </Row>
+</Container>
+</footer>
     )
   }
 }
