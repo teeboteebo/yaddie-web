771b617c30ae0037f2904fd15f252250b6ee1f11
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-09-18 14:35:47 +0200

    linked logo to home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './styles.scss';
-import { Row, Col, } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import SearchSection from '../SearchSection';
 import logo from '../../img/yaddie-logo-orange.png';
+import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <header>
-          <Row className="justify-content-md-center">
-            <Col xs={12} md={3} ><img className="mx-auto d-block logo" src={logo} alt={"logo"}/> </Col>
-          <Col xs={12} md={9} className="test"> <SearchSection /></Col>
+        <Row className='justify-content-md-center'>
+          <Col xs={12} md={3}>
+            <NavLink
+              to='/'
+              exact
+              className='mx-auto d-block logo'
+              src={logo}
+              alt={
+                'Orange tallrik med vita bestick i form av gaffel och kniv. Hemsidans namn Yaddie finns undertill tallriken i logan.'
+              }>
+              <img src={logo} />
+            </NavLink>{' '}
+          </Col>
+          <Col xs={12} md={9} className='test'>
+            {' '}
+            <SearchSection />
+          </Col>
         </Row>
-        
       </header>
-    )
+    );
   }
 }
-export default Header
+export default Header;
