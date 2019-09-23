46be608cce353bc530582d271a2669d3305f0e27
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-09-23 11:32:35 +0200

    added axios req

--- src/views/StartPage/index.js
+++ src/views/StartPage/index.js
@@ -3,10 +3,15 @@ import "./styles.scss"
 
 import RecipeLister from "../../components/RecipeLister"
 import ImageSlider from "../../components/ImageSlider"
+import axios from "axios"
 
 class StartPage extends React.Component {
   constructor() {
     super()
+    this.state = {
+      latestRecipes : []
+    }
+
     this.recipes = [
       {
         title: "Spaghetti Bolognese",
@@ -27,16 +32,32 @@ class StartPage extends React.Component {
           "Inte för den svaga typen. Extrem hetta men smakerna florerar och ger dig en upplevelse utan dess like. Det är det bästa pizzareceptet som någon någonsin har skapat!"
       }
     ]
+
+    this.getLatestRecipes()
   }
+
+  async getLatestRecipes(){
+    const latestRecipes = await axios({
+      method: 'GET',
+      url: `/api/recipes/latest`
+    })
+    await this.setState({latestRecipes})
+  }
+
   render() {
+    if(this.state.latestRecipes.data){
     return (
       <div className="start-page">
         <h3 className="mb-5 mb-md-3">Yaddie rekommenderar</h3>
         <ImageSlider />
         <h3 className="mb-5 mb-md-3">Senast tillagda recept</h3>
-        <RecipeLister recipes={this.recipes} />
+        <RecipeLister recipes={this.state.latestRecipes.data} />
       </div>
     )
+    }
+    else{
+      return(<div/>)
+    }
   }
 }
 
