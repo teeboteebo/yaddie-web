4b2b7c04f98dbc3bfb6fc22c7daefed9bedb3cb0
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-09-23 12:17:13 +0200

    erased hard coding from startpage

--- src/views/StartPage/index.js
+++ src/views/StartPage/index.js
@@ -12,27 +12,6 @@ class StartPage extends React.Component {
       latestRecipes : []
     }
 
-    this.recipes = [
-      {
-        title: "Spaghetti Bolognese",
-        img: "/images/food/pasta-bolog.jpg",
-        rating: 4.2,
-        tags: ["Middag", "Spaghetti", "Köttfärs"],
-        cookingTime: 85,
-        summary:
-          "Så här lagar du en klassisk förrätt från Bologna. I en riktig Bolognesesås ingår alltid buljong, morot och rotselleri. Det är dessa ingredienser som ger den alldeles speciella smaken till såsen. Servera med spaghetti och riven parmesanost."
-      },
-      {
-        title: "Pizza a la fantastico",
-        img: "/images/food/yummie-pizza.jpg",
-        rating: 4.9,
-        tags: ["Pizza", "Kött", "Machoman"],
-        cookingTime: 35,
-        summary:
-          "Inte för den svaga typen. Extrem hetta men smakerna florerar och ger dig en upplevelse utan dess like. Det är det bästa pizzareceptet som någon någonsin har skapat!"
-      }
-    ]
-
     this.getLatestRecipes()
   }
 
